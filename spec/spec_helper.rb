21917b48f8bcfc6b7be022ccd66db7829c3584ae
Phil Monroe <phil@philmonroe.com>
2013-06-20 21:21:09 -0700

    simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,19 @@
 ENV["RAILS_ENV"] ||= "test"
+
+require 'simplecov'
+require 'simplecov-rcov'
+SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
+SimpleCov.start 'rails' do
+  add_filter 'vendor'
+  add_filter '.bundle'
+
+  add_group "Models",      "app/models"
+  add_group "Controllers", "app/controllers"
+  add_group "Lib",         "lib"
+  add_group "Long files" do |src_file|
+    src_file.lines.count > 100
+  end
+end
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/autorun'
