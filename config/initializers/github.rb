28d83334e00d859b38c4a0ca054bcea5a1ba49c9
Phil Monroe <phil@philmonroe.com>
2013-06-21 09:11:12 -0700

    set a user agent

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -1,4 +1,6 @@
 GITHUB = Github.new do |config|
   config.client_id     = Conf::Github[:client_id]
   config.client_secret = Conf::Github[:client_secret]
+  config.user_agent    = 'Gistr - RBSavvy - phil@rbsavvy.com - ' + config.user_agent
+
 end
